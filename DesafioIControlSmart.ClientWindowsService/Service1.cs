f3514bd7d3b4bfe3bfd2a834d20f4b5c85d8e34b
Yan Paulo <yanpaulo@hotmail.com>
2019-03-30 13:38:24 -0300

    Service stub

---
+++ DesafioIControlSmart.ClientWindowsService/Service1.cs
@@ -0,0 +1,31 @@
+﻿using DesafioIControlSmart.Client;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesafioIControlSmart.ClientWindowsService
+{
+    public partial class Service1 : ServiceBase
+    {
+        public Service1()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            App.Instance.StartAsync().Wait();
+        }
+
+        protected override void OnStop()
+        {
+            App.Instance.StopAsync().Wait(); ;
+        }
+    }
+}
